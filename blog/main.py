7c532c5fb17ff128828412ee6036df95b2ea9197
blackswanalpha <kamandembugua18@gmail.com>
2022-05-06 09:55:55 +0300

    final update (learning)

---
+++ blog/main.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI, Depends, status, Response, HTTPException
+from sqlalchemy import delete
+from sqlalchemy.orm import Session
+from typing import List
+from . import schema, models
+from .database import engine, get_db
+
+models.Base.metadata.create_all(engine)
+
+app = FastAPI()
+
+
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
+def create(request: schema.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+@app.get('/blog',response_model=List[schema.ShowBlog])
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blog/{id}', response_model=schema.ShowBlog)
+def show(id, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=
+                            status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with the id {id} is not available")
+        # response.status_code =   status.HTTP_404_NOT_FOUND
+        # return {'detail': f"Blog with the id {id} is not available"}
+
+    return blog
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schema.Blog, db: Session = Depends(get_db)):
+    # db.query(models.Blog).filter(models.Blog.id == id).update({'title': 'updated tiltle'})
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with id {id} not found")
+    blog.update(request.dict())
+    db.commit()
+    return 'updated'
+
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id ==
+                                        id).delete(synchronize_session=False)
+
+    db.commit()
+    return {"done"}
--- main.py
+++ main.py
@@ -14,16 +14,56 @@
 #     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import uvicorn
 from fastapi import FastAPI
+from typing import Optional
+
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
 @app.get('/')
 def index():
-    return {"hey":{"name":"Tony"}}
+    return {"hey": {"name": "Tony"}}
 
 
 @app.get('/about')
 def about():
     return {'data': {'about page'}}
+
+
+app.get('/blog')
+
+
+def blog(limit=10, published: bool = True, sort: Optional[str] = None):
+    # only get 10 published blogs
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post('/blog/create')
+def create_blog(request: Blog):
+    return {'data': f"Blog is created with title as {request.title}"}
+
+
+# if __name__ == " __main__":
+#     uvicorn.run(app, host="127.0.0.1", port=9000)
